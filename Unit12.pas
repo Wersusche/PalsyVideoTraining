2f2ea3fed1769bb95964c2cb8da693fd957dc8b1
wersusche@gmail.com <Wersusche>
2023-03-28 23:57:09 +0800

    home

--- Unit12.pas
+++ Unit12.pas
@@ -132,6 +132,9 @@ FDConnection1.Close;
  FLibraryPath := GetPathWithVideo;
  //FillFilesList;
  ListTxtFiles;
+ //(trackbar.Max - trackBar.Position) + trackBar.Min;
+ // MediaPlayer1.Volume.MaxValue := tbVolume.Max;
+ //MediaPlayer1.Volume.MinValue :=  tbVolume.Min;
  tbVolume.Value := MediaPlayer1.Volume;
 end;
 
@@ -164,15 +167,16 @@ end;
 
 procedure TForm12.Timer3Timer(Sender: TObject);
 begin
+
   if tbProcess.Max <> MediaPlayer1.Duration then
     tbProcess.Max := MediaPlayer1.Duration;
   if tbProcess.Value <> MediaPlayer1.CurrentTime then
     tbProcess.Value := MediaPlayer1.CurrentTime;
 
   if (MediaPlayer1.Media = nil) or ((MediaPlayer1.State = TMediaState.Stopped) and (MediaPlayer1.CurrentTime >= MediaPlayer1.Duration)) then
-   Listbox1.ItemIndex:= Listbox1.ItemIndex + 1;
+   //Listbox1.ItemIndex:= Listbox1.ItemIndex + 1;
  //MediaPlayer1.FileName := TPath.Combine(Path, Listbox1.ItemIndex, Item.Text);
- ListBox1.Items.Strings[Listbox1.ItemIndex];
+ //ListBox1.Items.Strings[Listbox1.ItemIndex];
 end;
 
 procedure TForm12.FDConnection1BeforeConnect(Sender: TObject);
@@ -227,160 +231,19 @@ begin
 end;
 
 procedure TForm12.tbProcessChange(Sender: TObject);
-begin
- //MediaPlayer1.CurrentTime := tbProcess.Value;
+begin //MediaPlayer1.CurrentTime := tbProcess.Value;
 end;
 
 procedure TForm12.tbVolumeChange(Sender: TObject);
 begin
-MediaPlayer1.Volume := tbVolume.Value;
+MediaPlayer1.Volume := ((tbVolume.Max - tbVolume.value) + tbVolume.Min);
 end;
 
 end.
 
 
-//uses
-//  System.SysUtils, System.Types, System.UITypes, System.Classes,
-//  System.Variants,
-//  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TabControl,
-//  FMX.Media, FMX.Objects,
-//  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.ListBox;
-//
-//type
-//  TfrmMain = class(TForm)
-//    Rectangle1: TRectangle;
-//    MediaPlayerControl1: TMediaPlayerControl;
-//    MediaPlayer1: TMediaPlayer;
-//    Timer1: TTimer;
-//    tbVolume: TTrackBar;
-//    ScrollBox1: TScrollBox;
-//    ListBox1: TListBox;
-//    ListBoxGroupHeader1: TListBoxGroupHeader;
-//    bToTheStart: TButton;
-//    Panel1: TPanel;
-//    bPlay: TButton;
-//    bStop: TButton;
-//    bToTheEnd: TButton;
-//    b10SecBackward: TButton;
-//    b10SecForward: TButton;
-//    Panel2: TPanel;
-//    Panel3: TPanel;
-//    bMovingParentUp: TButton;
-//    bMovingParentDown: TButton;
-//    bChangeParentsVisible: TButton;
-//    tbProcess: TTrackBar;
-//    procedure bPlayClick(Sender: TObject);
-//    procedure FormCreate(Sender: TObject);
-//    procedure ListBox1ItemClick(const Sender: TCustomListBox;
-//      const Item: TListBoxItem);
-//    procedure bStopClick(Sender: TObject);
-//    procedure bToTheStartClick(Sender: TObject);
-//    procedure tbVolumeChange(Sender: TObject);
-//    procedure Timer1Timer(Sender: TObject);
-//    procedure bToTheEndClick(Sender: TObject);
-//    procedure b10SecBackwardClick(Sender: TObject);
-//    procedure b10SecForwardClick(Sender: TObject);
-//    procedure Panel1Click(Sender: TObject);
-//    procedure bMovingParentDownClick(Sender: TObject);
-//    procedure bMovingParentUpClick(Sender: TObject);
-//    procedure bChangeParentsVisibleClick(Sender: TObject);
-//  private
-//    { Private declarations }
-//    FLibraryPath: string;
-//    function GetPathWithVideo: string;
-//    procedure FillFilesList;
-//  public
-//    { Public declarations }
-//  end;
-//
-//var
-//  frmMain: TfrmMain;
-//
-//implementation
-//
-//uses
-//  System.IOUtils;
-//
-//const
-//  TenSecond = 100000000;
-//  StepForShifting = 10;
-//
-//{$R *.fmx}
-//
-////procedure TfrmMain.b10SecBackwardClick(Sender: TObject);
-//var
-//  LState: TMediaState;
-//begin
-//  LState := MediaPlayer1.State;
-//  if MediaPlayer1.CurrentTime > TenSecond then
-//  begin
-//    if LState = TMediaState.Playing then
-//      MediaPlayer1.Stop;
-//    MediaPlayer1.CurrentTime := MediaPlayer1.CurrentTime - TenSecond;
-//    if LState = TMediaState.Playing then
-//      MediaPlayer1.Play;
-//  end
-//  else
-//    MediaPlayer1.CurrentTime := 0;
-//end;
-
-//procedure TfrmMain.b10SecForwardClick(Sender: TObject);
-//var
-//  LState: TMediaState;
-//begin
-//  LState := MediaPlayer1.State;
-//  if MediaPlayer1.CurrentTime < (MediaPlayer1.Duration - TenSecond) then
-//  begin
-//    if LState = TMediaState.Playing then
-//      MediaPlayer1.Stop;
-//    MediaPlayer1.CurrentTime := MediaPlayer1.CurrentTime + TenSecond;
-//    if LState = TMediaState.Playing then
-//      MediaPlayer1.Play;
-//  end
-//  else
-//  begin
-//    MediaPlayer1.CurrentTime := MediaPlayer1.Duration;
-//  end;
-//end;
-
-//procedure TfrmMain.bChangeParentsVisibleClick(Sender: TObject);
-//begin
-//  Rectangle1.Visible := not Rectangle1.Visible;
-//end;
-//
-//procedure TfrmMain.bMovingParentDownClick(Sender: TObject);
-//begin
-//  Rectangle1.Position.Y := Rectangle1.Position.Y + StepForShifting;
-//end;
-//
-//procedure TfrmMain.bMovingParentUpClick(Sender: TObject);
-//begin
-//  Rectangle1.Position.Y := Rectangle1.Position.Y - StepForShifting;
-//end;
 
 
-
-procedure TfrmMain.bToTheEndClick(Sender: TObject);
-begin
-  MediaPlayer1.CurrentTime := MediaPlayer1.Duration;
-  MediaPlayer1.Stop;
-end;
-
-procedure TfrmMain.bToTheStartClick(Sender: TObject);
-begin
-  MediaPlayer1.CurrentTime := 0;
-end;
-
-
-
-
-
-procedure TfrmMain.Panel1Click(Sender: TObject);
-begin
-  FLibraryPath := GetPathWithVideo;
-  FillFilesList;
-  tbVolume.Value := MediaPlayer1.Volume;
-end;
 
 
 
