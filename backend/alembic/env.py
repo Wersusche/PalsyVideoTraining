951e988c7e1a151e262328f6d36ddc224f31cd61
Wersusche <129189272+Wersusche@users.noreply.github.com>
2025-09-24 15:54:45 +0800

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -16,7 +16,6 @@ from app.core.config import get_settings  # noqa: E402
 
 config = context.config
 settings = get_settings()
-config.set_main_option("sqlalchemy.url", settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
